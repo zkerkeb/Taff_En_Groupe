b0aead7f4e23af8bd52bbc15c25d2a142a6dce06
Zakaria Kerkeb <hikkary@mac.local>
2023-04-11 11:45:01 +0200

    eat(image): added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => alert('WAW')} >Wesh</button>
+      <img 
+        width={272}
+        height={92}
+        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+      ></img>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
